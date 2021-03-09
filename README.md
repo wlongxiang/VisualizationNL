d470d73af30b5f5097207dbe8a364b338393d710
Benjamin Wang <wlongxiang1119@gmail.com>
2021-03-09 16:32:08 +0100

    restructure

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ the Netherlands.
 
 - We use Apache Echarts to create interactive visulizations
 - We convert shapfiles or geopackage data to GeoJSON with geoPandas
+- kwb-2017 seems to have the most complete income data
+
+# Data
+- key figures wijken: https://www.cbs.nl/nl-nl/maatwerk/2020/29/kerncijfers-wijken-en-buurten-2020
+- Geometry data: https://www.cbs.nl/nl-nl/dossier/nederland-regionaal/geografische-data/wijk-en-buurtkaart-2020#:~:text=De%20Wijk%2D%20en%20Buurtkaart%202020,%E2%84%A2%2Dfgdb%20en%20geopackage).
+
--- js/amstelveen.js
+++ js/amstelveen.js
@@ -1,21 +1,22 @@
 var chartDom = document.getElementById('main');
 var myChart = echarts.init(chartDom);
 var option;
-var aantal_inwoners_data = [];
-
+var amstelveen_data = []; // column income_per_receiver, population
+var income_data = [];
 myChart.showLoading();
 
 // load data
-$.getJSON('./data/processed/cities/amstelveen/amstelveen-aantal-inwoners.json', function(data){
- aantal_inwoners_data = data;
+$.getJSON('./data/processed/cities/amstelveen/amstelveen-data.json', function(data){
+ amstelveen_data = data;
 });
 
 $.get('./data/processed/cities/amstelveen/amstelveen-geometry.json', function (amstelveenJson) {
     myChart.hideLoading();
     echarts.registerMap('amstelveen', amstelveenJson); //register geojson as map
+    console.log(amstelveenJson);
     option = {
         title: {
-            text: 'Amstelveen 2020',
+            text: 'Amstelveen Visulization',
             subtext: 'Data From CBS',
             sublink: 'https://www.cbs.nl/nl-nl/dossier/nederland-regionaal/geografische-data/wijk-en-buurtkaart-2020#:~:text=De%20Wijk%2D%20en%20Buurtkaart%202020,%E2%84%A2%2Dfgdb%20en%20geopackage).',
             left: 'center'
@@ -26,16 +27,17 @@ $.get('./data/processed/cities/amstelveen/amstelveen-geometry.json', function (a
             transitionDuration: 0.2,
             formatter: function (params) {
                 console.log(params);
-                console.log(params.value);
-                var value = (params.value + '').split('.');
-                value = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
+                console.log(params.data.value);
+                var value = JSON.stringify(params.data.value);
+//                value = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
                 return params.seriesName + '<br/>' + params.name + ': ' + value;
             }
         },
         visualMap: {
             left: 'right',
-            min: 0,
-            max: 5000,
+//            min: 0,
+            max: 80,
+            dimension: 0, // set which column is used to create heatmap
             inRange: {
                 color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
             },
@@ -54,7 +56,7 @@ $.get('./data/processed/cities/amstelveen/amstelveen-geometry.json', function (a
         },
         series: [
             {
-                name: 'Population',
+                name: 'AmstelveenData',
                 type: 'map',
                 roam: true,
                 map: 'amstelveen',
@@ -64,7 +66,7 @@ $.get('./data/processed/cities/amstelveen/amstelveen-geometry.json', function (a
                         show: true
                     }
                 },
-                data:aantal_inwoners_data
+                data: amstelveen_data
             }
         ]
 
